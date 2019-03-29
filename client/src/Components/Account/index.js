fc7a17c788d0e727b31f89f0de1deb9895500a55
Tasha <shradertasha@gmail.com>
2019-03-29 14:36:05 -0500

    nav

---
+++ client/src/Components/Account/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+
+function Account() {
+    return(
+        
+        <div className="account">
+        <p>
+            this will be account page
+        </p>
+        
+        </div>
+        
+    )
+}
+
+export default Account;
