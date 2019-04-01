e89102fa3b6e166bc0857a7b1e53a3e357a79f97
Tasha <shradertasha@gmail.com>
2019-04-01 12:09:57 -0500

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Account from "./Components/Account";
