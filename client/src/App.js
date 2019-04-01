8fa2000611de170c3c59281b2949a1ac856322c3
Tasha <shradertasha@gmail.com>
2019-04-01 13:17:30 -0500

    login

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Account from "./Components/Account";
-import NavBar from "./Components/NavBar";
-
+import LogIn from "./Components/SignIn";
 class App extends Component {
   render() {
     return (
       <Router>
       <div>
-      <NavBar />
-      {/* <Route exact path="/" component={LogIn} /> */}
+      <Route exact path="/login" component={LogIn} />
       {/* <Route exact path="/home" component={Home} /> */}
       <Route exact path="/account" component={Account} />
       </div>
