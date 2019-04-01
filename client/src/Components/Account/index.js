49008e2fac1bd3750513f989d0bee1cfa0e7a754
Tasha <shradertasha@gmail.com>
2019-04-01 11:13:11 -0500

    account page

--- client/src/Components/Account/index.js
+++ client/src/Components/Account/index.js
@@ -3,14 +3,19 @@ import "./style.css";
 
 
 function Account() {
-    return(
-        <div className="account">
-        <p>
-            this will be account page
-        </p>
-        
+    return (
+        <div>
+            <div className="row">
+                <div className="card1">displaying some user info</div>
+                <div className="card2">card2</div>
+                <div className="card3">card3</div>
+                <div className="card4">card4</div>
+                <div className="card5">card5</div>
+            </div>
+            <div className="account">
+                <p>this will be account page</p>
+            </div>
         </div>
-        
     )
 }
 
