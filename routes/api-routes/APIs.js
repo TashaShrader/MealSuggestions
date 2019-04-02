4a13cd116d5a7ed4d4335fd4dc7218afb4f30092
mitburr <mitburr@gmail.com>
2019-04-02 09:27:29 -0500

    some oauth setup

---
+++ routes/api-routes/APIs.js
@@ -0,0 +1,94 @@
+//dependency
+var express = require('express')
+var mongojs = require("mongojs")
+var axios = require("axios");
+var Book = require("../../models/Book")
+var mongoose = require("mongoose");
+
+//WE SHOULD REALLY ADD A CONTROLLER !!!
+
+//setup router access
+var app = express.Router();
+
+// Database configuration
+mongoose.connect("mongodb://localhost/users", { useNewUrlParser: true });
+
+
+
+
+
+
+
+app.post('/login', function (req, res) {
+    //Normally, you would fetch the user from the database here.
+    //then save the relevant info in user details
+    db.person.findOne({
+        where: {
+            userID: req.body.Username,
+        },
+    }).then((function (userObj) {
+        //test for userObj being found
+        if (!userObj) { return res.sendStatus(404) }
+
+
+        else if (userObj.password === req.body.Password) {
+            console.log(userObj.id);
+
+            user = {
+                Username: userObj.userID,
+                Password: userObj.password,
+            }
+
+            jwt.sign(user, JWT_SECRET_KEY, JWT_OPTIONS,
+                function (err, token) {
+                    if (err) return res.sendStatus(500).json(err) //do some error checking
+                    else {
+                        res.json({
+                            user,
+                            Token: token,
+                        })
+                    }
+
+                })
+        }
+        else {
+            res.sendStatus(401).send();
+        }
+    })).catch(function (err) {
+        return res.sendStatus(500).json(err)
+    });
+})
+
+let verifyToken = function (req, res, next) {
+    //declare the token from each request. 
+    //The token is sent automatically in a cookie,
+    // cookie_parser allows us to read the cookie easily
+    const token = req.cookies.Token;
+    //first determine if a token is found
+    if (token) {
+        //with this nested if I verify the token. 
+        //the callback in jwt.verify will return true if verified w/ jwt's verified.
+        //the contents of the if statement are actually just the next() function
+        if (jwt.verify(token, JWT_SECRET_KEY, function (err, data) {
+            //self explanatory: not verified => forbidden.
+            //interestingly, this does succesfully end the verifyToken function as if it was returned. 
+            if (err) { res.sendStatus(403) }
+            //return true to fulfill the if, token verified for testing
+            else {
+                console.log("token verified")
+                return true
+            }
+        })
+        ); {
+            //next() is what makes the middleware function continue to the next function
+            next();
+        }
+    } else {
+        //403 means forbidden, slightly stronger than unauthorized.
+        //this is else if the token is not found in cookies. 
+        res.sendStatus(403);
+    }
+}
+
+
+  module.exports = app;
---
+++ server.js
@@ -0,0 +1,46 @@
+//BOILERPLATE AND REQUIRES
+const express = require("express");
+const path = require("path");
+// const apiRoutes = require('./routes/api-routes')
+// const htmlRoutes = require('./routes/html-routes')
+var mongoose = require("mongoose")
+//express server opened
+const app = express();
+//|------------------------------------------------------------------------------------------------|
+
+//HEROKU DEPLOYMENT SERVER BOILERPLATE
+
+//or instantiated to open a port for Heroku
+const PORT = process.env.PORT || 3001;
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//|------------------------------------------------------------------------------------------------|
+// DATABASE CONFIGURATION: MONGO
+var databaseUrl = "MealSearch";
+var collections = ["users"];
+
+// Hook mongojs configuration to the db variable
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/" + databaseUrl;
+
+mongoose.connect(MONGODB_URI);
+
+//|------------------------------------------------------------------------------------------------|
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Define API apiRoutes here
+// app.use(apiRoutes);
+// Send every other request to the React app
+// Define any API apiRoutes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
