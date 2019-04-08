a9c26526b80dcb1f2d5809a369d23ebb772fa0b5
Tasha <shradertasha@gmail.com>
2019-04-08 13:07:49 -0500

    hi

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use(express.json());
 // app.use(apiRoutes);
 // Send every other request to the React app
 // Define any API apiRoutes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
