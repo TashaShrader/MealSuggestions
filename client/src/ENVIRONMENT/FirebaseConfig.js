e7a85a13ef44099e958159211145bbe3ee527a4d
Tasha <shradertasha@gmail.com>
2019-04-08 15:22:09 -0500

    heroku

--- client/src/ENVIRONMENT/FirebaseConfig.js
+++ client/src/ENVIRONMENT/FirebaseConfig.js
@@ -5,7 +5,7 @@ const config = {
   "projectId": "meal-suggestions",
   "storageBucket": "meal-suggestions.appspot.com",
   "messagingSenderId": "710323756283",
-  "FIREBASE_ADMIN_USER": "mjburr6797@gmail.com",
-  "FIREBASE_ADMIN_PASS": "12315141514mjb"
+  // "FIREBASE_ADMIN_USER": "mjburr6797@gmail.com",
+  // "FIREBASE_ADMIN_PASS": "12315141514mjb"
 }
 export default config;
