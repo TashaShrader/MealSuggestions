56c9fb06b8ab51b48e16ff60e373d4c181d53508
Thao <thaotou@amazon.com>
2019-04-09 19:40:38 -0500

    cleanedSomeCode

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -68,41 +68,38 @@ class Home extends Component {
           />
           <button onClick={this.handleFormSubmit} >Submit</button>
         </form>
+
+      {/* Will need to add entered meals to meal logs for the day */}
+
         <div className="container">
           <section className="col-md-12 content" id="home">
             <div className="col-lg-6 col-md-6 content-item tm-black-translucent-bg tm-logo-box">
               <h1 className="text-uppercase">Your Log</h1>
               <div className="div-table">
                 <div className="div-table-row">
-                  <div className="div-table-col">Foods Ate Today </div>
+                  <div className="div-table-col"></div>
                 </div>
                 <div className="div-table-row">
-                  <div className="div-table-col">Pizza Slice</div>
+                  <div className="div-table-col"></div>
                 </div>
                 <div className="div-table-row">
-                  <div className="div-table-col">Hamburger</div>
+                  <div className="div-table-col"></div>
                 </div>
                 <div className="div-table-row">
-                  <div className="div-table-col">Petri Dish</div>
+                  <div className="div-table-col"></div>
                 </div>
               </div>
             </div>
             <div className="col-lg-6 col-md-6 content-item content-item-1 background tm-white-translucent-bg">
               <h2 className="main-title text-center dark-blue-text">The best way to lose weight is to track what you eat!</h2>
-              <p>Sed tempus, quam vitae lobortis vulputate, sapien nisi ultricies metus, in eleifend ipsum mauris eget magna. Nullam massa nisi, pellentesque eu pretium quis, pulvinar a nulla. Sed tempus finibus lacus, vestibulum condimentum nibh blandit vel.</p>
+              {/* <p>Sed tempus, quam vitae lobortis vulputate, sapien nisi ultricies metus, in eleifend ipsum mauris eget magna. Nullam massa nisi, pellentesque eu pretium quis, pulvinar a nulla. Sed tempus finibus lacus, vestibulum condimentum nibh blandit vel.</p> */}
             </div>
           </section>
-          <section className="col-md-12 content padding tm-equal-col-container" id="services">
-            <div className="col-lg-6 col-md-6 col-sm-12 content-item tm-black-translucent-bg tm-services-section-padding">
-              <button type="button" name="mealtime" className="btn btn-big pull-right tm-btn-primary">Log New Meal</button>
-            </div>
-            <div className="col-lg-6 col-md-6 col-sm-12 tm-white-translucent-bg tm-services-section-padding tm-box-center">
-            </div></section>
         </div>
         <div className="text-center footer">
-          <div className="container">
+          {/* <div className="container">
             Copyright © <span className="tm-current-year">2019</span> Case Pollock
-                    </div>
+                    </div> */}
         </div>
       </div>
     );
--- client/src/Components/NavBar/index.js
+++ client/src/Components/NavBar/index.js
@@ -8,7 +8,7 @@ import 'firebase/auth';
 function NavBar() {
     return (
         <div className="navbar">
-            <div className="title">Meal Suggestion App</div>
+            <div className="title">The Mealler Report</div>
             <a href="/home" className="home">home</a>
             <a href="/account" className="accountbutton">account</a>
             <a href="/" className="logout" onClick={() => firebase.auth().signOut()}>sign-out</a>
